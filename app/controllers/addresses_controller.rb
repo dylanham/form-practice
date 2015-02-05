2f4bf6219600118f4a568f7800ae05ef73eaf25c
Dylan Ham <dylanham@gmail.com>
2015-02-04 21:17:40 -0700

    user should see forms edit and be able to create new ones

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,43 @@
+class AddressesController < ApplicationController
+  def index
+    @addresses = Address.all
+  end
+  def new
+    @address = Address.new
+  end
+  def create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to addresses_path
+    else
+
+      render :new
+    end
+  end
+  def edit
+    @address = Address.find(params[:id])
+
+  end
+
+  def show
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.new(address_params)
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
+  end
+
+
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :address_line_one, :address_line_two, :city, :state, :zipcode, :phone_number)
+  end
+
+end
